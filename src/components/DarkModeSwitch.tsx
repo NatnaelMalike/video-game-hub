780201727ab21e80c3c640ab88eca3fed6a4b93c
natnael <natnaelmalike@gmail.com>
2024-09-23 23:56:45 +0300

    Build dark mode switch

---
+++ src/components/DarkModeSwitch.tsx
@@ -0,0 +1,13 @@
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+
+const DarkModeSwitch = () => {
+    const {toggleColorMode, colorMode} = useColorMode()
+  return (
+    <HStack>
+        <Switch colorScheme={'teal'} isChecked ={colorMode == 'dark'} onChange={toggleColorMode}/>
+        <Text>Dark Mode</Text>
+    </HStack>
+  )
+}
+
+export default DarkModeSwitch
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { HStack, Image } from "@chakra-ui/react"
 import logo from '../assets/logo.webp'
+import DarkModeSwitch from "./DarkModeSwitch"
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={'space-between'} padding={'16px'}>
         <Image src={logo} boxSize={"64px"}/>
-        <Text>Navbar</Text>
+        <DarkModeSwitch/>
     </HStack>
   )
 }
