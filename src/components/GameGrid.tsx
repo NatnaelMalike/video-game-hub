b781c8fe82ef1dd3566a52faf8005bbfcd5c95a3
natnael <natnaelmalike@gmail.com>
2024-09-25 02:03:39 +0300

    Get optimized images

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
         <>
             {loading && <Text>Loading...</Text>}
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm:1, md: 2, lg: 3, xl: 4}} padding={'10px'} gap={10}>
+            <SimpleGrid columns={{sm:1, md: 2, lg: 3, xl: 5}} padding={'10px'} gap={10}>
                 {games.map((game) => (
                     <GameCard key={game.id} game={game}/>
                 ))}
