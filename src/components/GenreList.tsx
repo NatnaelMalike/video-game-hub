b7bcfb096f48ed945b125607ad97defe66463be0
natnael <natnaelmalike@gmail.com>
2024-09-26 15:51:41 +0300

    Displaying genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,20 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenres";
+import getCroppedImgUrl from "../services/image-url";
 
 const GenreList = () => {
     const { data } = useGenre();
     return (
-        <div>
-            <ul>
-                {data.map((genre) => (
-                    <li key={genre.id}>{genre.name}</li>
-                ))}
-            </ul>
-        </div>
+        <List>
+            {data.map((genre) => (
+                <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='40px' src={getCroppedImgUrl(genre.image_background)} borderRadius={10}/>
+                        <Text fontSize='lg'>{genre.name}</Text>
+                    </HStack>
+                    </ListItem>
+            ))}
+        </List>
     );
 };
 
